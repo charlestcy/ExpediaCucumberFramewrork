3559a90b890c6813471c3957314f2db174ae8e0e
charles <charlescy2501@gmail.com>
2022-05-17 13:44:35 +0100

    adding initial cucumber project

---
+++ src/test/java/steps/SearchResultSteps.java
@@ -0,0 +1,10 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+
+public class SearchResultSteps {
+    @Then("I should be able to view list of Hotel Search result")
+    public void iShouldBeAbleToViewListOfHotelSearchResult() {
+        System.out.println("I should be able to view list of Hotel Search result");
+    }
+}
